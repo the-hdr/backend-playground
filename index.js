1db82fadb955424edf84a0c4533dee2b7ebc299e
Mohd Abbas Haidar <the-hdr@users.noreply.github.com>
2023-10-17 12:11:19 +0530

    add comment
    Added comments to make the code more understandable

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
+// just the boiler-plate code with some changes:
+
+/* The changes basically include converting arrow functions to named
+   functions.
+*/ 
+
 const express = require('express')
 const app = express()
 const port = 3000
 
-function handleFirstRequest (req, res)
+app.get('/', handleGetRequest)  // accepting get requests on the '/' route;
+app.listen (port, startListening) // binding a port to listen to
+                                  // the specified host and port;
+
+function handleGetRequest (req, res)
 {
     res.send ('Hello, world!')
 }
 
-function start ()
+function startListening ()
 {
     console.log (`Example app listening on port ${port}`)
 }
-
-app.get('/', handleFirstRequest)
-app.listen (port, start)
 
 /*
 app.listen(port, () => {
